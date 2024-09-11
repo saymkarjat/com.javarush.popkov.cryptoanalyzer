377893f30a7bf46e6c2f2e808447f40124b6be43
saymkarjat <70850092+saymkarjat@users.noreply.github.com>
2024-09-12 01:19:46 +0300

    исправлены ошибки, изменен статистический анализ в методе брут форс, вместо частоты букв, считаем триграммы и биграммы

--- src/main/java/org/example/com_javarush_popkov_cryptoanalyzer/BruteForce.java
+++ src/main/java/org/example/com_javarush_popkov_cryptoanalyzer/BruteForce.java
@@ -1,20 +1,48 @@
 package org.example.com_javarush_popkov_cryptoanalyzer;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 
 public class BruteForce {
+    public static List<String> substrings = Arrays.asList("СТ", "НО", "ЕН", "ТО", "НА", "ОВ", "НИ", "РА", "ВО", "КО",
+            "СТО", "ЕНО", "НОВ", "ТОВ", "ОВО", "ОВА");
+
     public static String decryptByBruteForce(String s) {
         // Логика brute force
         ArrayList<Integer> matches = new ArrayList<>();
 
+
         for (int i = 1; i <= Cipher.ALPHABET.length; i++) {
-            String temp = Cipher.decrypt(s,i);
-
+            int count = 0;
+            String temp = Cipher.decrypt(s, i);
+            count = allMatches(temp.toUpperCase(), substrings);
+            matches.add(count);
         }
-        int maxValue = Collections.max(matches);
+        int max = Collections.max(matches);
+        int key = matches.indexOf(max);
+        return Cipher.decrypt(s, key + 1);
+    }
 
+    private static int countMatches(String text, String substring) {
+        int count = 0;
+        int index = 0;
+        String ourText = text.toUpperCase();
+        while ((index = ourText.indexOf(substring, index)) != -1) {
+            count++;
+            index += substring.length();
+        }
+        return count;
+    }
 
-        return Cipher.decrypt(s,1);
+    public static int allMatches(String text, List<String> substrings) {
+        int count = 0;
+        String ourText = text.toUpperCase();
+        for (String string : substrings) {
+
+            count += countMatches(ourText, string);
+        }
+        return count;
     }
 }
